dec7451e8131f9903746dd04809b0e9455e89db5
mar1n <30738216+mar1n@users.noreply.github.com>
2020-02-06 04:38:55 +0000

    almost there again

--- src/components/QueenBee/QueenBee.js
+++ src/components/QueenBee/QueenBee.js
@@ -12,13 +12,25 @@ export default function QueenBee() {
     const [dronebee, setDronebee] = useState({
         points: 50,
         quantity: 8
-    })
+    });
+    const [reset, setReset] = useState({
+        message: 'Play, Game!'
+    });
     const hitQueen = () => {
         //setQueen({points: queen.points - 8});
         const random = Math.floor(Math.random() * 3);
         if (random === 0) {
             if (queen.points - 8 < 0) {
                 setQueen({ points: 0 })
+                setReset({ message: 'Wait 2s reset is on the way!' })
+                setTimeout(function reset() {
+                    //setReset({ message: 'Play, Game!'});
+                    console.log('szymon');
+                    setQueen({ points: 100, quantity: 1 });
+                    setWorkerbee({ points: 75, quantity: 5 });
+                    setDronebee({ points: 50, quantity: 8 });
+                    setReset({ message: 'Play, Game!' });
+                }, 2000)
             } else {
                 setQueen({ ...queen, points: queen.points - 8 });
             }
@@ -34,33 +46,30 @@ export default function QueenBee() {
                 setWorkerbee({ ...workerbee, points: workerbee.points - 10 });
             }
         }
-        
-            if (dronebee.points - 12 === 0) {
-                setDronebee({ ...dronebee, points: 50, quantity: dronebee.quantity - 1 });
-            } else if (dronebee.points < 12) {
-                setDronebee({ ...dronebee, points: dronebee.points - 12 + 50, quantity: dronebee.quantity - 1 });
-            } else {
-                setDronebee({ ...dronebee, points: dronebee.points - 12 })
-            }
-        
+
+        if (dronebee.points - 12 === 0) {
+            setDronebee({ ...dronebee, points: 50, quantity: dronebee.quantity - 1 });
+        } else if (dronebee.points < 12) {
+            setDronebee({ ...dronebee, points: dronebee.points - 12 + 50, quantity: dronebee.quantity - 1 });
+        } else {
+            setDronebee({ ...dronebee, points: dronebee.points - 12 })
+        }
+
     }
     return (
         <>
-            <p>{queen.points}</p>
-            {
-                Array(queen.quantity).fill(1).map(queen => <div key={queen} className='queen'></div>)
-            }
-            {
-                Array(workerbee.quantity).fill(1).map(worker => <div key={worker} className='worker'></div>)
-            }
-            {workerbee.quantity >= 0 &&
-                <p>{workerbee.points} {workerbee.quantity}</p>
-            }
-            {dronebee.quantity > 0 && dronebee.quantity !== 0 &&
-                <p>{dronebee.points} {dronebee.quantity}</p>
-            }
-            {Array(dronebee.quantity).fill(1).map(dronebee => <div key={dronebee} className='dronebee'></div>)}
-            <button onClick={hitQueen}>Hit</button>
+            <div className='container'>
+                <h1>{reset.message}</h1>
+                {queen.points !== 0 && Array(queen.quantity).fill(1).map(queen => <div key={queen} className='queen'></div>)}
+                {workerbee.quantity >= 0 &&
+
+                    Array(workerbee.quantity).fill(1).map(worker => <div key={worker} className='worker'></div>)
+                }
+                {dronebee.quantity > 0 && dronebee.quantity !== 0 &&
+                    Array(dronebee.quantity).fill(1).map(dronebee => <div key={dronebee} className='dronebee'></div>)
+                }
+                <button onClick={hitQueen} >Hit</button>
+            </div>
         </>
     );
 }
